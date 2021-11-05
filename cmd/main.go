cfb06707385856581c1a3b99fc2f90ef2939e6df
Виталий Шульман <vitalian42@mail.ru>
2021-11-05 14:41:03 +0300

    Add config

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,62 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/ValeryBMSTU/evoModeler/internal/api"
+	yaml "gopkg.in/yaml.v2"
 )
+
+type Config struct {
+	Host string `yaml1:"host"`
+	Port string `yaml1:"port"`
+}
+
+func ParseFlags() (string, error) {
+	var configPath string
+	flag.StringVar(&configPath, "config", "./config.yaml", "path to config file")
+	flag.Parse()
+	fmt.Println(configPath)
+	return configPath, nil
+}
+
+func NewConfig(configPath string) (*Config, error) {
+	config := &Config{}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	d := yaml.NewDecoder(file)
+	if err := d.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
 
 func main() {
 	api.DevPrint()
 
+	cfgPath, err := ParseFlags()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg, err := NewConfig(cfgPath)
+	if err != nil {
+		log.Fatal()
+	}
+
 	http.HandleFunc("/ping", api.PingHandler)
 	http.HandleFunc("/", api.DoNothingHandler)
 
-	fmt.Println("starting server at :8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("starting server at " + cfg.Host + ":" + cfg.Port)
+	http.ListenAndServe(cfg.Host+":"+cfg.Port, nil)
 }
